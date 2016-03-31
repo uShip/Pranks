f12b3d8b01a80ebb7ed2007aea8fc63c2bb2fa50
Stephen <rguerra712@yahoo.com>
2016-03-31 09:02:18 -0500

    Updated help page
    Updated the help page to be more descriptive and useful.

--- addhost.ps1
+++ addhost.ps1
@@ -1,9 +1,29 @@
-# A script used to add aliases to ones host file automatically
+<#
+
+.SYNOPSIS
+This is a script used to add aliases to ones host file automatically
+
+.DESCRIPTION
+The way this file works, it will find the path of your host file, 
+and append the IP of the site -redirectto when a person goes to the site -site
+
+.EXAMPLE
+./addhost.ps1 -site stackoverflow.com -redirectto www.myspace.com
+
+.NOTES
+If you experience access issues, try running the following script as administrator.
+You may need to run `Set-ExecutionPolicy RemoteSigned` before running any powershell commands.
+The site 'redirectto' MUST be accessible via IP address.
+
+.LINK
+https://github.com/rguerra712/Pranks
+
+#>
+
+# 
 # 
 # NOTES: 
-# If you experience access issues, try running the following script as administrator.
-# You may need to run `Set-ExecutionPolicy RemoteSigned` before running any powershell commands.
-# The site 'redirectto' MUST be accessible via IP address.
+# 
 
 param (
     [string]$site = $(throw "-site is required."),
