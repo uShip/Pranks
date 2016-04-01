77817daecad96ae39513f451336df2623ccb3a87
Stephen Guerra <rguerra712>
2016-03-31 19:35:26 -0500

    Added example of how to Prank C# Math Constants
    Setting Math.PI to value of 3 in an example based on user name.

---
+++ IndianaPi/IndianaPi/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IndianaPi
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("PI is {0}", Math.PI);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IndianaPi/IndianaPi/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IndianaPi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IndianaPi")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5066efa9-516d-4777-98b0-5f66ac2f3d64")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+internal class Math
+{
+    private const string Prakee = "{Insert Prankee Username here}";
+    public static double PI = 
+        Environment.UserName.Equals(Prakee, StringComparison.InvariantCultureIgnoreCase) 
+        ? 3.0 
+        : System.Math.PI;
+}
